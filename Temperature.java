a72143dae5a0a3e5f4fe266e8127a8c51746e2e8
Asiyacm <asiyacmuhammed@gmail.com>
2022-10-25 22:49:16 +0530

    initial commit

---
+++ Temperature.java
@@ -0,0 +1,8 @@
+public class Temperature {
+    public static void main(String[] args)
+    {
+        float Fahrenheit=176, Celsius=0;
+        Celsius  = ((Fahrenheit-32)*5)/9;
+        System.out.println("celsius value is: "+Celsius);
+    }
+}
